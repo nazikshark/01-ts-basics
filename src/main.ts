9de5240ed9d8a3138c58872aad6e7d7da3791a0a
nazikshark <nazarpro783@gmail.com>
2025-12-28 12:50:17 +0100

    fyghg

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './task-01'; 
-import './task-02'; 
-import './task-03'; 
-import './task-04'; 
-import './task-05'; 
-import './task-06'; 
-import './task-07'; 
-import './task-08'; 
+import './task-1'; 
+import './task-2'; 
+import './task-3'; 
+import './task-4'; 
+import './task-5'; 
+import './task-6'; 
+import './task-7'; 
+import './task-8'; 
